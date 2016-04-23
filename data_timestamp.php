eb22d6a766abc6f2f2ba575c294dfd4553dfd4b5
dilson-bd <dilson.ti@outlook.com>
2016-04-23 03:58:24 -0300

    Update data_timestamp.php

--- data_timestamp.php
+++ data_timestamp.php
@@ -1,5 +1,17 @@
+<?php
+//Trechos estão sem nexos, pois é apenas anotação para ser revista no futuro por mim OK.
 $this->db->distinct();
 $this->db->select("club_id")
 $this->db->limit($limit);
 $this->db->where("UNIX_TIMESTAMP(`end_date`) > ".time());
 $q = $this->db->get('offers');
+
+//Outro fragmento para ser estudado
+ $this->db->select('*');
+    $this->db->select("DATE_FORMAT( date, '%d.%m.%Y' ) as date_human",  FALSE );
+    $this->db->select("DATE_FORMAT( date, '%H:%i') as time_human",      FALSE );
+
+
+    $this->db->from('news');
+
+    $this->db->where('news_id', $news_id );
